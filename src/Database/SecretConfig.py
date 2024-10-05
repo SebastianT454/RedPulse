3c800f36218fb6cfe521eacb637bc1a7f7f527bc
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2024-10-05 18:02:07 -0500

    Add files via upload

---
+++ src/Database/SecretConfig.py
@@ -0,0 +1,5 @@
+PGHOST='ep-icy-snowflake-a5l92gnv.us-east-2.aws.neon.tech'
+PGDATABASE='MotorDeEncriptacion_DB'
+PGUSER='MotorDeEncriptacion_DB_owner'
+PGPASSWORD='0nRvJp4WMQtE'
+PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR EN SU DB
