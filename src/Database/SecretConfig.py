535eb0f5c7847ce497e9efc4960f78a0e44ca018
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2024-10-05 18:13:17 -0500

    Actualizar SecretConfig.py

--- src/Database/SecretConfig.py
+++ src/Database/SecretConfig.py
@@ -1,5 +1,5 @@
-PGHOST='ep-icy-snowflake-a5l92gnv.us-east-2.aws.neon.tech'
-PGDATABASE='MotorDeEncriptacion_DB'
-PGUSER='MotorDeEncriptacion_DB_owner'
-PGPASSWORD='0nRvJp4WMQtE'
+PGHOST=''
+PGDATABASE=''
+PGUSER=''
+PGPASSWORD=''
 PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR EN SU DB
