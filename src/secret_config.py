f92974916c56f3e15fa9b39bba46e8ac4d34729a
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2024-11-02 14:57:26 -0500

    Secret Config actualizado

---
+++ src/secret_config.py
@@ -0,0 +1,30 @@
+# Parametros para conexion con la BD (Base de Datos)
+PGHOST= 'ep-tight-field-a55txm1f.us-east-2.aws.neon.tech'
+PGDATABASE= 'redpulsedb'
+PGUSER= 'redpulsedb_owner'
+PGPASSWORD= 'eMOW3i8XPvmo'
+PGPORT = 5432 # POR DEFECTO ES 5432, PERO PUEDE CAMBIAR SI NO SE UTILIZA NEON TECH.
+
+# Token para bot de notificaciones.
+NOTIEMAIL = "noreplyredpulse@gmail.com"
+NOTICONTRA = "redpulse.123"
+
+# Secret key para el apartado web (Flask)
+
+SECRET_KEY_FLASK = "j98h3y2qweRTY8uiopASDFGHJKLZXCVBNM,./1234567890-=`~!@#$%^&*()_+[]{}|;:?><"
+
+# Key para la conexion con MetaAI # https://groq.com/groqcloud/
+CHAT_BOT_KEY = "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"
+
+# Key para la conexion con ChatGpt
+OLD_CHAT_BOT_KEY = "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"
+
+# Correo por defecto del administrador
+ADMINEMAIL = "redpulsetst@gmail.com"
+
+# Imagen de perfil por defecto para cada usuario
+DEFAULT_PROFILE_PICTURE = "https://i.pinimg.com/474x/65/25/a0/6525a08f1df98a2e3a545fe2ace4be47.jpg"
+
+# Keys para Imgur
+IMGUR_CLIENT_ID = "a16e9e8cb650a1b"
+IMGUR_CLIENT_SECRET = "dcd1c09918ebcc679c8516672f317ff6c82a8ece"
