ded651ce4d5e831cca26eb9392698b061d68dbaf
Sebastián T. <98891697+SebastianT454@users.noreply.github.com>
2024-11-11 18:44:08 -0500

    Quitar modo debug en app.py

--- src/app.py
+++ src/app.py
@@ -394,4 +394,4 @@ def reestablecer_contrasena():
     return render_template('reestablecer_contrasena.html')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
